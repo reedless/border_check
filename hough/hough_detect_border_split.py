9ea11bc6f402e1bbbbda4eb88027054131db0c27
Howard <howardyanghaowen@gmail.com>
2021-12-30 17:12:40 +0800

    edperimented with filtering hough centers (failure), annotated and created train-val-test split for 2k dataset

--- hough/hough_detect_border_split.py
+++ hough/hough_detect_border_split.py
@@ -4,14 +4,33 @@ import numpy as np
 import os
 
 def main():
-    loop(130, 20)
+    # for i in range(130, 201, 10):
+    #     print(i)
+    #     loop(i, 5000, 5000)
+    best = 0
+    res = (0, 0, 0, 0, 0)
+    for threshold in range(120, 151, 10):
+        for max_vert_lines in range(5, 21, 5):
+            for max_hor_lines in range(5, 21, 5):
+                print(f"threshold: {threshold}, max_vert_lines: {max_vert_lines}, max_hor_lines: {max_hor_lines}")
+                TP, TN, FP, FN = loop(threshold, max_vert_lines, max_hor_lines)
+                metric = TP*9 + TN
+                if metric > best:
+                    best = metric
+                    print(f"TP: {TP}, TN: {TN}, FP: {FP}, FN: {FN}")
+                    res = ( TP, TN, threshold, max_vert_lines, max_hor_lines)
+
+    TP, TN, threshold, max_vert_lines, max_hor_lines = res
+    print(f"threshold={threshold}, max_vert_lines={max_vert_lines}, max_hor_lines={max_hor_lines}")
+    print(f"TP: {TP}, TN: {TN}, FP: {1802-TN}, FN: {200-TP}")
+
     # best = 0
     # res = (0, 0, 0, 0)
-    # for threshold in range(50, 141, 5):
-    #     for max_lines in range(10, 51, 5):
+    # for threshold in range(50, 141, 10):
+    #     for max_lines in range(10, 51, 10):
     #         print(threshold, max_lines)
     #         TP, TN, _, _ = loop(threshold, max_lines)
-    #         metric = TP*18 + TN
+    #         metric = TP*9 + TN
     #         if metric > best:
     #             best = metric
     #             print(TP, TN)
@@ -21,7 +40,7 @@ def main():
     # print(f"threshold={threshold}, max_lines={max_lines}")
     # print(f"TP: {TP}, TN: {TN}, FP: {1802-TN}, FN: {200-TP}")
 
-def loop(threshold, max_lines):
+def loop(threshold, max_vert_lines, max_hor_lines):
 
     TP = 0
     TN = 0
@@ -38,34 +57,76 @@ def loop(threshold, max_lines):
         if i[-10:] == '_lines.jpg':
             continue
         filename = f"{test_dir}/{i}"
-        lines, cdst = check_border(filename, threshold)
+        vert_lines, hor_lines, cdst = check_border(filename, threshold)
 
         if i[:6] == 'border':
-            if lines is None or len(lines) > max_lines:
+            if not has_border(vert_lines, hor_lines, max_vert_lines, max_hor_lines, cdst):
                 # print(f"{i} is incorrect")
-                # if lines is not None:
-                #     print(len(lines))
+                # print_len_lines(vert_lines, hor_lines)
                 FN += 1
             else:
-                # print(len(lines))
+                # print(f"{i} is correct")
+                # print_len_lines(vert_lines, hor_lines)
                 TP += 1
         else:
-            if lines is not None and len(lines) <= max_lines:
+            if has_border(vert_lines, hor_lines, max_vert_lines, max_hor_lines, cdst):
                 # print(f"{i} is incorrect")
-                # print(len(lines))
+                # print_len_lines(vert_lines, hor_lines)
                 FP += 1
             else:
-                # if lines is not None:
-                #     print(len(lines))
                 TN += 1
 
     print(f"TP: {TP}, TN: {TN}, FP: {FP}, FN: {FN}")
     return TP, TN, FP, FN
+
+def has_lines_in_center(dst, lines):
+    for i in range(len(lines)):
+        # add to new_lines if line is at edge
+        rho = lines[i][0][0]
+        theta = lines[i][0][1]
+        (x1, y1), (x2, y2) = compute_line_endpoints(rho, theta)
+        height, width, _ = dst.shape
+
+        if not not_in_center_of_image(x1, y1, x2, y2, width, height):
+            return True
+
+    return False
+
+def has_border(vert_lines, hor_lines, max_vert_lines, max_hor_lines, cdst):
+    # no lines found
+    if vert_lines is None and hor_lines is None:
+        return False
+
+    # only hor lines found
+    if vert_lines is None:
+        if has_lines_in_center(cdst, hor_lines):
+            return False
+        return len(hor_lines) < max_vert_lines
+
+    # only vert lines found
+    if hor_lines is None:
+        if has_lines_in_center(cdst, vert_lines):
+            return False
+        return len(vert_lines) < max_hor_lines
+
+    # both lines found
+    if has_lines_in_center(cdst, vert_lines) or has_lines_in_center(cdst, hor_lines):
+        return False
+    return len(vert_lines) < max_vert_lines and len(hor_lines) < max_hor_lines
+
+def print_len_lines(vert_lines, hor_lines):
+    if vert_lines is not None:
+        print(len(vert_lines))
+    if hor_lines is not None:
+        print(len(hor_lines))
     
 def check_border(filename, threshold=100):
 
     src = cv.imread(filename, cv.IMREAD_GRAYSCALE)
-    dst = cv.Canny(src, 50, 200, None, 3)
+    high_thresh, _ = cv.threshold(src, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    lowThresh = 0.5*high_thresh
+    dst = cv.Canny(src, lowThresh, high_thresh, None, 3)
+    # dst = cv.Canny(src, 50, 200, None, 3)
     cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
 
     # vertical lines
@@ -81,10 +142,14 @@ def check_border(filename, threshold=100):
     else:
         vert_lines = None
 
+    # vert_lines = filter_only_lines_at_edges(dst, vert_lines)
+    
     # horizontal lines
     hor_lines = cv.HoughLines(dst, rho=1, theta=np.pi/180, threshold=threshold, min_theta=np.pi/180*85, max_theta=np.pi/180*95)
 
-    # concat vertical and horizxonal lines if they are not none
+    # hor_lines = filter_only_lines_at_edges(dst, hor_lines)
+
+    # concat vertical and horizonal lines if they are not none
     if vert_lines is not None:
         if hor_lines is not None:
             lines = np.concatenate((vert_lines, hor_lines))
@@ -96,18 +161,48 @@ def check_border(filename, threshold=100):
         lines = None
 
     if lines is not None:
-        for i in range(0, len(lines)):
+        for i in range(len(lines)):
             rho = lines[i][0][0]
             theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            pt1, pt2 = compute_line_endpoints(rho, theta)
             cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
 
-    return lines, cdst
+    return vert_lines, hor_lines, cdst
+
+def filter_only_lines_at_edges(dst, lines):
+    if lines is not None:
+        new_lines = np.zeros(lines.shape)
+        j = 0
+        for i in range(len(lines)):
+            # add to new_lines if line is at edge
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            (x1, y1), (x2, y2) = compute_line_endpoints(rho, theta)
+            height, width = dst.shape
+
+            if not_in_center_of_image(x1, y1, x2, y2, width, height):
+                new_lines[j] = lines[i]
+                j += 1
+
+        return new_lines[:j,:,:]
+
+    else:
+        return lines
+
+def not_in_center_of_image(x1, y1, x2, y2, width, height, factor=0.2):
+    return (x1 < width*factor or x1 > width*(1-factor)) and (x2 < width*factor or x2 > width*(1-factor)) and \
+            (y1 < height*factor or y1 > height*(1-factor)) and (y2 < height*factor or y2 > height*(1-factor))
+
+def compute_line_endpoints(rho, theta):
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+    return (x1, y1), (x2, y2)
     
     
     
